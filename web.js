e4c6e52c73f664b54a84f72913185349dbb7aad0
Nael <nerdupgeekdown@gmail.com>
2013-07-27 23:27:27 -0500

    refactored index.html

--- web.js
+++ web.js
@@ -8,6 +8,17 @@ app.get('/', function(request, response) {
   response.send(text);
 });
 
+app.get('/old', function(request, response) {
+    var text = fs.readFileSync('index2.html').toString();
+    response.send(text);
+});
+
+app.get('/example', function(request, response) {
+    var text = fs.readFileSync('index3.html').toString();
+    response.send(text);
+});
+
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
